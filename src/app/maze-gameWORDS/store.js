48e5b4d13c1b1a03e17fe2cec5d1d55e8251c4e8
Ed S <dsspab90000@gmail.com>
2025-05-30 11:56:06 -0300

    change numbers

--- src/app/maze-gameWORDS/store.js
+++ src/app/maze-gameWORDS/store.js
@@ -11,7 +11,7 @@
 //code 2
 import { create } from 'zustand';
 export const useStore = create((set) => ({
-  coins: 2000,
+  coins: 7040,
   loseCoins: (amount) =>
     set((state) => ({
       coins: Math.max(state.coins - amount, 0),
